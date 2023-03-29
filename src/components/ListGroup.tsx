ffa61831578e733d4a185bc44783e7e3d6b03031
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-03-29 20:11:58 +0100

    Interface and passing props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { MouseEvent, useState } from "react";
 
-const ListGroup = () => {
+interface Props {
+  fruits: string[];
+  heading: string;
+}
+
+const ListGroup = (props: Props) => {
   const fruits = ["Banana", "Orange", "Apple", "Mango"];
   //   const fruits: any = [];
 
-  let fruitsMapped = fruits.map((fruit: any) => {
-    return <li key={fruit}>{fruit}</li>;
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [name, setName] = useState("");
+
+  let fruitsMapped = fruits.map((fruit: any, index: number) => {
+    return (
+      <li
+        className="list-group-item"
+        key={fruit}
+        onClick={() => {
+          console.log(`You clicked on ${fruit}`, index);
+        }}
+      >
+        {fruit}
+      </li>
+    );
   });
 
   //   const message = fruits.length === 0 ? <p>No items</p> : null;
@@ -14,12 +33,55 @@ const ListGroup = () => {
     return fruits.length === 0 ? <p>No items</p> : null;
   };
 
+  // Event handler
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
+
   return (
     <>
-      <h1>List Group</h1>
+      <h1>List Group 1</h1>
       {getMessage()} {/* if ERROR display message */}
+      {fruits.length === 0 && <p>No items</p>}
+      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
+      <ul className="list-group">{fruitsMapped}</ul>
+      <h1>List Group 2</h1>
+      {fruits.length === 0 && <p>No items</p>}
+      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
       <ul className="list-group">
-        <li className="list-group-item">{fruitsMapped}</li>
+        {fruits.map((fruit: any, index: number) => {
+          return (
+            <li
+              className="list-group-item"
+              key={fruit}
+              onClick={() => {
+                console.log(`You clicked on ${fruit}`, index);
+              }}
+            >
+              {fruit}
+            </li>
+          );
+        })}
+      </ul>
+      <h1>List Group 3</h1>
+      {fruits.length === 0 && <p>No items</p>}
+      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
+      <ul className="list-group">
+        {fruits.map((fruit: any, index: number) => {
+          return (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={fruit}
+              onClick={() => setSelectedIndex(index)}
+            >
+              {fruit}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
