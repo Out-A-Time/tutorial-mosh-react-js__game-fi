619dac3d0aa0bfacf21152f7f36220017938f034
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-03-31 22:01:15 +0100

    Repeating theory

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,93 +1,25 @@
-import React, { MouseEvent, useState } from "react";
-
-// Props are inputs to our component
-interface Props {
-  kasztany: string[];
-  heading: string;
-  onSelectKasztany: (kasztany: string[]) => void;
-}
-
-const ListGroup = ({ kasztany, heading, onSelectKasztany }: Props) => {
-  const fruits = ["Banana", "Orange", "Apple", "Mango"];
-  //   const fruits: any = [];
-
-  // useState Hook - this component can have data or state that will change over time
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [name, setName] = useState("");
-
-  let fruitsMapped = fruits.map((fruit: any, index: number) => {
-    return (
-      <li
-        className="list-group-item"
-        key={fruit}
-        onClick={() => {
-          console.log(`You clicked on ${fruit}`, index);
-        }}
-      >
-        {fruit}
-      </li>
-    );
-  });
-
-  //   const message = fruits.length === 0 ? <p>No items</p> : null;
+import { MouseEvent } from "react";
+const ListGroup = () => {
+  let fruits = ["Banana", "Orange", "Apple", "Mango"];
+  // fruits = []
 
   const getMessage = () => {
     return fruits.length === 0 ? <p>No items</p> : null;
   };
 
-  // Event handler
   const handleClick = (event: MouseEvent) => {
     console.log(event);
-  };
+  }
 
   return (
     <>
-      <h1>List Group 1 {heading}</h1>
-      {getMessage()} {/* if ERROR display message */}
+      <h1>List Group</h1>
+      {/* if ERROR display message 1way */}
+      {getMessage()}
+      {/* if ERROR display message 2way*/}
       {fruits.length === 0 && <p>No items</p>}
-      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
-      <ul className="list-group">{fruitsMapped}</ul>
-      {/*  */}
-      <h1>List Group 2</h1>
-      {fruits.length === 0 && <p>No items</p>}
-      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
       <ul className="list-group">
-        {fruits.map((fruit: any, index: number) => {
-          return (
-            <li
-              className="list-group-item"
-              key={fruit}
-              onClick={() => {
-                console.log(`You clicked on ${fruit}`, index);
-              }}
-            >
-              {fruit}
-            </li>
-          );
-        })}
-      </ul>
-      {/* This has been passed as props from APP parent */}
-      <ul>
-        {kasztany.map((kasztan: any, index: number) => {
-          return (
-            <li
-              className="list-group-item"
-              key={kasztan}
-              onClick={() => {
-                console.log(`You clicked on ${kasztan}`, index);
-                onSelectKasztany(kasztan);
-              }}
-            >
-              {kasztan}
-            </li>
-          );
-        })}
-      </ul>
-      <h1>List Group 3</h1>
-      {fruits.length === 0 && <p>No items</p>}{" "}
-      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
-      <ul className="list-group">
-        {fruits.map((fruit: any, index: number) => {
+        {fruits.map((fruit, index) => {
           return (
             <li className="list-group-item" key={fruit} onClick={handleClick}>
               {fruit}
@@ -95,29 +27,6 @@ const ListGroup = ({ kasztany, heading, onSelectKasztany }: Props) => {
           );
         })}
       </ul>
-      {/*  */}
-      <h1>List Group 4</h1>
-      {fruits.length === 0 && <p>No items</p>}
-      {kasztany.length === 0 && <p>No kasztans</p>}
-      {/* if ERROR display message ->  true && 'Mosh' returns 'Mosh'*/}
-      <ul className="list-group">
-        {fruits.map((fruit: string, index: number) => {
-          return (
-            <li
-              className={
-                selectedIndex === index
-                  ? "list-group-item active"
-                  : "list-group-item"
-              }
-              key={fruit}
-              onClick={() => setSelectedIndex(index)}
-            >
-              {fruit}
-            </li>
-          );
-        })}
-      </ul>
-      <p>=================================</p>
     </>
   );
 };
